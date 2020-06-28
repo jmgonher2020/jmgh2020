051be8841b1f74187e2254d8424386d44fdc1d13
jmgonzalez@aserta.com.es <jmgonzalez@aserta.com.es>
2020-06-28 19:30:16 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "Servicio_ip" {
   value = "${kubernetes_service.echo.load_balancer_ingress.0.ip}"
 }
 output "Puerto" {
-  value = "${kubernetes_service.echo.spec.0.port.target_port}"
+  value = "${kubernetes_service.echo.spec.0.target_port}"
 }  
 output "Replicas" {
   value = "${kubernetes_deployment.echo.spec.0.replicas}"
