9dff70bb325f798be29e79b898553bad0ee2a74e
jmgonher2020 <62194140+jmgonher2020@users.noreply.github.com>
2020-06-30 12:25:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,2 @@
 provider "kubernetes" {
-  #config_path = "/var/lib/jenkins/config"
-  #config_path = "${data.oci_containerengine_cluster_kube_config.cluster_kube_config.config_file_path}"
-  #config_context_auth_info = "target-cluster"
 }
