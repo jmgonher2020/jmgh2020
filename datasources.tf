5464621d7663d1a71c27fe27865743ac1c9dbdc3
jmgonher2020 <62194140+jmgonher2020@users.noreply.github.com>
2020-06-30 12:30:36 -0500

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -1,8 +1,10 @@
 data "oci_identity_availability_domains" "ADs" {
-  compartment_id = "ocid1.compartment.oc1..aaaaaaaauow2wnn5drbtbuaak7s2h54ok746k7r4waw3ujfgl4eh2sv73ipa"
+  compartment_id = "${var.compartment_ocid}"
+  #compartment_id = "ocid1.compartment.oc1..aaaaaaaauow2wnn5drbtbuaak7s2h54ok746k7r4waw3ujfgl4eh2sv73ipa"
 }
 data "oci_core_images" "oracle_linux_image" {
-  compartment_id           = "ocid1.compartment.oc1..aaaaaaaauow2wnn5drbtbuaak7s2h54ok746k7r4waw3ujfgl4eh2sv73ipa"
+  "compartment_id           = "ocid1.compartment.oc1..aaaaaaaauow2wnn5drbtbuaak7s2h54ok746k7r4waw3ujfgl4eh2sv73ipa"
+  compartment_id = "${var.compartment_ocid}"
   operating_system         = "Oracle Linux"
   operating_system_version = "7.6"
 }
